b2eb9de0ade41795025668a219e28999253eb914
FIY-pc <2953997758@qq.com>
2024-10-04 23:48:37 +0800

    feat(ODB-Lite/data/autoSaver): Add essential auto-save API

--- ODB-Lite/server/data/include/autoSaver.h
+++ ODB-Lite/server/data/include/autoSaver.h
@@ -9,11 +9,22 @@
 
 // 用外部变量记录变化数
 extern int change_count;
+// 用外部变量控制线程关闭
+extern int autoSaverSwitch;
 // 操作变化数的函数
 void increment_change_count();
 void reset_change_count();
 int get_change_count();
 
-void AutoSaver_create(HashTable ht,SDS time,SDS changeNum);
+// 变化监视器线程相关函数与定义
+typedef struct
+{
+    HashTable *ht;
+    SDS time;
+    SDS changeNum;
+    char fileName[128];
+}autoSaverArgs;
 
+SDS autoSaver_create(HashTable *ht, const char *filename,SDS time,SDS changeNum);
+void *autoSaver(void *arg);
 #endif //AUTOSAVER_H
--- ODB-Lite/server/data/src/autoSaver.c
+++ ODB-Lite/server/data/src/autoSaver.c
@@ -4,10 +4,13 @@
 
 #include "autoSaver.h"
 
+#include <string.h>
+#include <pthread.h>
 #include <unistd.h>
+#include <bits/pthreadtypes.h>
 // 定义全局变量
 int change_count = 0;
-
+int autoSaverSwitch = 0;
 // 增加变化数
 void increment_change_count() {
     change_count++;
@@ -21,7 +24,45 @@ int get_change_count() {
     return change_count;
 }
 // 变化监控器线程创建
-void AutoSaver_create(HashTable ht,SDS time,SDS changeNum)
+SDS autoSaver_create(HashTable *ht, const char *filename,SDS time,SDS changeNum)
 {
+    if (strcmp(time.data,"0") == 0 || strcmp(changeNum.data,"0") == 0)
+    {
+        return sds_new("autoSaver not be created because auto saving is closed");
+    }
+    // 打开开关
+    autoSaverSwitch = 1;
+    // 传参结构体
+    autoSaverArgs args;
+    args.ht = ht;
+    args.time = time;
+    args.changeNum = changeNum;
+    strcpy(args.fileName,filename);
 
+    pthread_t autoSaver_thread;
+    pthread_create(&autoSaver_thread,NULL,autoSaver,(void*)&args);
+    return sds_new("autoSaver created");
+}
+
+// 变化监控器线程函数
+void *autoSaver(void *arg)
+{
+    autoSaverArgs *args = (autoSaverArgs *)arg;
+    SDS SDStime = args->time;
+    SDS SDSchangeNum = args->changeNum;
+    int time,changeNum;
+    sscanf(SDStime.data,"%ld",&time);
+    sscanf(SDSchangeNum.data,"%ld",&changeNum);
+    while (1)
+    {
+        if(autoSaverSwitch==0)
+        {
+            pthread_exit(NULL);
+        }
+        if(change_count>=changeNum)
+        {
+            odbrgsave(args->ht,args->fileName);
+        }
+        sleep(time);
+    }
 }
